3c22b42267dfd596e53417798b8871ac79a99cab
13B8B <108516478+reed192512@users.noreply.github.com>
2023-11-28 20:46:42 +0800

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 6872265039 then
+	loadstring(game:HttpGet("PUT BEDWARS LOBBY HEREhttps://raw.githubusercontent.com/reed192512/Jayyware-bedwars/main/bedwarslobby.lua", true))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/reed192512/Jayyware-bedwars/main/betarelease.lua", true))()
+end
